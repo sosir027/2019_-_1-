954623f1450965d6b6ac9dd66981bcb38334450e
hyeeun <35446821+sosir027@users.noreply.github.com>
2019-10-31 16:29:57 +0900

    crawler_20191031
    happy halloween :-3

--- crawler.py
+++ crawler.py
@@ -9,7 +9,8 @@ grade3_ce = 'http://www2.sungshin.ac.kr/ce/11804/subview.do?enc=Zm5jdDF8QEB8JTJG
 grade4_ce = 'http://www2.sungshin.ac.kr/ce/11804/subview.do?enc=Zm5jdDF8QEB8JTJGY3JjbG0lMkZjZSUyRjA0JTJGYXJ0Y2xMaXN0LmRvJTNGeWVhciUzRDIwMTklMjY%3D#none'
 
 ce = [grade1_ce, grade2_ce, grade3_ce, grade4_ce]
-ce_datas = []
+columns = ['FIELD', 'SUBNAME', 'CREDIT']
+ce_datas = [columns]
 
 for url in ce:
     req = requests.get(url)
@@ -19,7 +20,8 @@ for url in ce:
     subs = soup.select('tbody > tr')
 
     for sub in subs:
-        ce_datas.append(list(filter(None, re.split('  |\t|\n', sub.text)))[:-2])
+        ce_datas.append(list(filter(lambda x: x != '' and x != ' ', re.split('  |\t|\n|\r', sub.text)))[:-2])
+        # ce_datas.append(list(filter(None, re.split('  |\t|\n', sub.text)))[:-2])
         print(ce_datas)
 
 dataFrame = pd.DataFrame(ce_datas)
